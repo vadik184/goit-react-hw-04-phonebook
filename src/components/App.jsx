519f24ae4d65e2f8883676f51a0e2ee091b3eb5a
vadik184 <133372327+vadik184@users.noreply.github.com>
2023-12-03 22:00:59 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { nanoid } from 'nanoid';
 import { Section } from 'components/Section/Section';
@@ -9,81 +9,66 @@ import { Filter } from 'components/Filter/Filter';
 import { StyledContainer } from 'components/AppStyle';
 
 import { formatPhoneNumber, formatName } from '../helpers/script';
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount() {
-    const contactsList = localStorage.getItem('contactsList');
-    const parsContacts = JSON.parse(contactsList);
+const storageContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? storageContacts
+  );
 
-    if (contactsList !== null) {
-      this.setState({ contacts: parsContacts });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contactsList', JSON.stringify(this.state.contacts));
-    }
-  }
-  deleteContact = contactID => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== contactID),
-    }));
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = contactID => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactID)
+    );
   };
-  formSubmitHandler = data => {
+  const formSubmitHandler = data => {
     const dataName = data.name;
 
     if (
-      this.state.contacts.some(
+      contacts.some(
         contact =>
           contact.name.toLocaleLowerCase() === dataName.toLocaleLowerCase()
       )
     ) {
       return alert(`${dataName} is already in contacts`);
     }
-    return this.setState(prev => ({
-      ...prev,
-      contacts: [
-        ...prev.contacts,
-        {
-          id: nanoid(),
-          name: dataName.split(' ').map(formatName).join(' '),
-          number: formatPhoneNumber(data.number),
-        },
-      ],
-    }));
+    setContacts(prevContacts => [
+      ...prevContacts,
+      {
+        id: nanoid(),
+        name: dataName.split(' ').map(formatName).join(' '),
+        number: formatPhoneNumber(data.number),
+      },
+    ]);
   };
 
-  chageFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const chageFilter = e => {
+    setFilter(e.currentTarget.value);
   };
-  InputId = nanoid();
-  render() {
-    const selectedByFilter = this.state.contacts.filter(contact =>
-      contact.name
-        .toLocaleLowerCase()
-        .includes(this.state.filter.toLocaleLowerCase())
-    );
-    return (
-      <StyledContainer>
-        <Section title="Phonebook">
-          <Form onSubmit={this.formSubmitHandler} />
-        </Section>
-        <Section title="Contacts">
-          <Filter filter={this.state.filter} onChageFilter={this.chageFilter} />
-          <Contacts
-            contacts={selectedByFilter}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
-      </StyledContainer>
-    );
-  }
-}
+
+  const selectedByFilter = contacts.filter(contact =>
+    contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+  );
+
+  return (
+    <StyledContainer>
+      <Section title="Phonebook">
+        <Form onSubmit={formSubmitHandler} />
+      </Section>
+      <Section title="Contacts">
+        <Filter filter={filter} onChageFilter={chageFilter} />
+        <Contacts contacts={selectedByFilter} onDeleteContact={deleteContact} />
+      </Section>
+    </StyledContainer>
+  );
+};
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import {
   StyledButten,
@@ -6,72 +6,138 @@ import {
   StyledFormLabel,
   StyledForm,
 } from 'components/Form/FormStyle';
-const initState = {
-  name: '',
-  number: '',
-};
-export class Form extends Component {
-  state = { ...initState };
 
-  handelSubmit = e => {
+export const Form = ({ onSubmit }) => {
+  const initState = {
+    name: '',
+    number: '',
+  };
+
+  const [formData, setFormData] = useState({ ...initState });
+
+  const handelSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit({ ...this.state });
-    this.resetForm();
+    onSubmit({ ...formData });
+    resetForm();
   };
-  resetForm = () => {
-    this.setState({ ...initState });
+  const resetForm = () => {
+    setFormData({ ...initState });
   };
-  handelChange = e => {
+  const handelChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+    setFormData(prevData => ({ ...prevData, [name]: value }));
   };
-  nameInputId = nanoid();
-  numberInputId = nanoid();
+  const nameInputId = nanoid();
+  const numberInputId = nanoid();
 
-  render() {
-    return (
-      <StyledForm onSubmit={this.handelSubmit}>
-        <StyledFormLabel
-          htmlFor={this.nameInputId}
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'start',
-          }}
-        >
-          Name
-          <StyledFormIput
-            type="text"
-            name="name"
-            id={this.nameInputId}
-            value={this.state.name}
-            placeholder="Enter name"
-            onChange={this.handelChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            required
-          />
-        </StyledFormLabel>
-        <StyledFormLabel
-          htmlFor={this.numberInputId}
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'start',
-          }}
-        >
-          Number
-          <StyledFormIput
-            type="tel"
-            name="number"
-            id={this.numberInputId}
-            value={this.state.number}
-            placeholder="Enter 7-digit number"
-            onChange={this.handelChange}
-            pattern="[0-9]{7}"
-          />
-        </StyledFormLabel>
-        <StyledButten type="submit">Add contact</StyledButten>
-      </StyledForm>
-    );
-  }
-}
+  return (
+    <StyledForm onSubmit={handelSubmit}>
+      <StyledFormLabel
+        htmlFor={nameInputId}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'start',
+        }}
+      >
+        Name
+        <StyledFormIput
+          type="text"
+          name="name"
+          id={nameInputId}
+          value={formData.name}
+          placeholder="Enter name"
+          onChange={handelChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          required
+        />
+      </StyledFormLabel>
+      <StyledFormLabel
+        htmlFor={numberInputId}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'start',
+        }}
+      >
+        Number
+        <StyledFormIput
+          type="tel"
+          name="number"
+          id={numberInputId}
+          value={formData.number}
+          placeholder="Enter 7-digit number"
+          onChange={handelChange}
+          pattern="[0-9]{7}"
+        />
+      </StyledFormLabel>
+      <StyledButten type="submit">Add contact</StyledButten>
+    </StyledForm>
+  );
+};
+
+// export class Form extends Component {
+//   state = { ...initState };
+
+//   handelSubmit = e => {
+//     e.preventDefault();
+//     this.props.onSubmit({ ...this.state });
+//     this.resetForm();
+//   };
+//   resetForm = () => {
+//     this.setState({ ...initState });
+//   };
+//   handelChange = e => {
+//     const { name, value } = e.currentTarget;
+//     this.setState({ [name]: value });
+//   };
+//   nameInputId = nanoid();
+//   numberInputId = nanoid();
+
+//   render() {
+//     return (
+//       <StyledForm onSubmit={this.handelSubmit}>
+//         <StyledFormLabel
+//           htmlFor={this.nameInputId}
+//           style={{
+//             display: 'flex',
+//             flexDirection: 'column',
+//             alignItems: 'start',
+//           }}
+//         >
+//           Name
+//           <StyledFormIput
+//             type="text"
+//             name="name"
+//             id={this.nameInputId}
+//             value={this.state.name}
+//             placeholder="Enter name"
+//             onChange={this.handelChange}
+//             pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             required
+//           />
+//         </StyledFormLabel>
+//         <StyledFormLabel
+//           htmlFor={this.numberInputId}
+//           style={{
+//             display: 'flex',
+//             flexDirection: 'column',
+//             alignItems: 'start',
+//           }}
+//         >
+//           Number
+//           <StyledFormIput
+//             type="tel"
+//             name="number"
+//             id={this.numberInputId}
+//             value={this.state.number}
+//             placeholder="Enter 7-digit number"
+//             onChange={this.handelChange}
+//             pattern="[0-9]{7}"
+//           />
+//         </StyledFormLabel>
+//         <StyledButten type="submit">Add contact</StyledButten>
+//       </StyledForm>
+//     );
+//   }
+// }
